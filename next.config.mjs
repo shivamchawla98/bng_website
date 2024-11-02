1d9dfe80283838fe763278cd396320b8051745fd
Shivam Chawla <chawlas123456@gmail.com>
2024-11-02 19:10:30 +0530

    correction to nofollow noindex configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Robots-Tag',
+                        value: 'noindex, nofollow'
+                    }
+                ]
+            }
+        ]
+    }
 };
 
 export default nextConfig;
+
