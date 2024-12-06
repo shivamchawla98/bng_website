31205af2c605d2063155418c2d0227af149f4c82
Shivam Chawla <chawlas123456@gmail.com>
2024-12-07 03:53:37 +0530

    changes

--- src/page-components/Contact/MapComponent.jsx
+++ src/page-components/Contact/MapComponent.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 const MapComponent = ({ address }) => {
+  const apiKey = process.env.MAP_API_KEY;
+
   return (
     <div className="w-full h-96 bg-gray-200">
-      {/* Map Placeholder */}
       <iframe
         title="map"
         className="w-full h-full"
-        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBBSMdTJBK6ZY1GB1MNFhcT67x5F8h-N5I&q=${encodeURIComponent(address)}`}
+        // width="600"
+        height="600"
+        style={{ border: 0 }}
+        loading="lazy"
         allowFullScreen
-      />
+        src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${encodeURIComponent(address)}`}
+      ></iframe>
     </div>
   );
 };
