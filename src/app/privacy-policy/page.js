7d9b48c3303712d018f689312df9125d0789471a
abhishek bharti <abhishek@abhisheks-MacBook-Air.local>
2025-03-18 22:09:48 +0530

    added privacy policy and use policy pages

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='m-8'>
+        <h2 className='text-2xl font-semibold text-gray-700'>
+        Privacy Policy
+        </h2>
+  <h3 className='text-gray-700 font-semibold text-base'>
+    
+ABOUT :
+    </h3>      
+
+<p className='text-sm text-gray-800'>
+
+
+This Privacy Policy describes the terms of commitment of the website HYPERLINK "http://www.bnglogisticsnetwork.com"http://www.bnglogisticsnetwork.com  to your privacy. The policy statement is applicable for all information gathering and broadcasting practices followed by BNG Info Media And Technologies LLP, for the people visiting HYPERLINK "http://www.bnglogisticsnetwork.com"http://www.bnglogisticsnetwork.com   by using the site, you consent to collect and broadcast of information in terms of the policy below.  By visiting the website HYPERLINK "http://www.bnglogisticsnetwork.com"http://www.bnglogisticsnetwork.com   announce that you have accepted the terms of the privacy policy.   
+
+BNG LOGISTICS NETWORK is dedicated to protecting your personal information and will make every reasonable effort to handle collected information appropriately. All information collected will be handled with care in accordance with BNG LOGISTICS NETWORK standards for integrity and objectivity and respect for your privacy. BNG LOGISTICS NETWORK endeavours to comply with all laws and regulations that apply to the gathering and use of personal information, including US privacy laws and the EU General Data Protection Regulation. This privacy policy describes the information we collect, the purposes for which it is used, and your choices regarding its use. As used in this privacy policy, “we” and “us” includes BNG LOGISTICS NETWORK and our staff, employees, officers, directors, volunteer editors and reviewers, and vendors and independent contractors under contract with us. “HYPERLINK "http://www.bnglogisticsnetwork.com"http://www.bnglogisticsnetwork.com” includes the following: the web pages and content on the BNG LOGISTICS NETWORK Website HYPERLINK "http://www.bnglogisticsnetwork.com"http://www.bnglogisticsnetwork.com, journal sites, and BNG LOGISTICS NETWORK or third party tools, software, submission systems, web forms, registration system, and any other means through which we interact with you.
+
+BNG LOGISTICS NETWORK is committed to protecting your privacy seriously and will use your personal information to administer your account and to provide the product and service you request from BNG LOGISTICS NETWORK, all the services  include but are not limited to provide you with B2B, B2C, with national or international Freight Forwarders, Logistics Companies, Supply Chain Service Providers, Exporters, Importers, Sellers , Buyers, Manufacturers, traders and or intermediaries,
+
+BNG LOGISTICS NETWORK may contact you with the promotional offers and deals, products and services via various suitable communication channels such as Email, SMS, Telephone or Fax, WhatsApp or Short Message Services and or via our automated call systems. 
+
+</p>
+    </div>
+  )
+}
+
+export default page
