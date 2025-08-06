4f5566df2a55892d378d99ee94bc5e607a38e34f
Shivam Chawla <chawlas123456@gmail.com>
2025-08-07 02:47:15 +0530

    removing back the env property from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -10,11 +10,6 @@ const nextConfig = {
                         value: 'noindex, nofollow'
                     }
                 ],
-                env: {
-                    API_KEY: process.env.API_KEY,
-                    GRAPHQL_URI: process.env.GRAPHQL_URI,
-                    API_ACCESS_EMAIL: process.env.API_ACCESS_EMAIL,
-                },
             }
         ]
     }
