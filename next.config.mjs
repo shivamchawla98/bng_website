d78597d797ea966fb1c674bc4db28e903ab63f28
Shivam Chawla <chawlas123456@gmail.com>
2025-08-07 02:41:47 +0530

    attempting env fix

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,12 @@ const nextConfig = {
                         key: 'X-Robots-Tag',
                         value: 'noindex, nofollow'
                     }
-                ]
+                ],
+                env: {
+                    API_KEY: process.env.API_KEY,
+                    GRAPHQL_URI: process.env.GRAPHQL_URI,
+                    API_ACCESS_EMAIL: process.env.API_ACCESS_EMAIL,
+                },
             }
         ]
     }
