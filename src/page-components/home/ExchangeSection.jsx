0f815b52ffb02e9a9a1982802a3b3baaea615163
Shivam Chawla <chawlas123456@gmail.com>
2026-02-16 00:51:15 +0530

    last 2 changes

--- src/page-components/home/ExchangeSection.jsx
+++ src/page-components/home/ExchangeSection.jsx
@@ -91,7 +91,7 @@ const ExchangeSection = () => {
 
   return (
     <section className="bg-[#EFF6FF] py-16 lg:py-24">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-[100rem] mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
         <div className="text-center mb-12 relative">
           <h2 className="text-4xl lg:text-[55px] font-bold text-gray-900 mb-4">
